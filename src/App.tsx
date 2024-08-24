164e1ca7068f7768f0d1c3dc1d44c7d73edcf100
arseniypom <arpomazkov@bk.ru>
2024-08-24 16:22:46 +0300

    Added real data display

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import ArticlesList from './components/ArticlesList';
 import { Typography } from '@mui/material';
 import './App.css';
+import { IArticlesResponse } from './types/article';
+import { useAxiosRequest } from './hooks/useAxiosRequest';
 
 function App() {
-  const [topic, setTopic] = useState('');
+  const { data, isLoading, error, makeRequest } =
+    useAxiosRequest<IArticlesResponse>();
+
+  const [topic, setTopic] = useState({ value: '', isDirty: false });
 
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTopic(e.target.value);
+    setTopic({ value: e.target.value, isDirty: true });
   };
+
+  useEffect(() => {
+    if (!topic.isDirty) {
+      return;
+    }
+
+    const fetchArticles = async (topic: string) => {
+      await makeRequest({
+        url: 'http://localhost:3000/articles',
+        method: 'GET',
+        params: { topic },
+      });
+    };
+
+    const timeoutId = setTimeout(() => fetchArticles(topic.value), 500);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [topic, makeRequest]);
 
   return (
     <Box
@@ -19,16 +44,43 @@ function App() {
         maxWidth: '100%',
       }}
     >
-      <Typography component="h1" variant="h2" gutterBottom>
-        Articles Seeker
-      </Typography>
-      <TextField
-        fullWidth
-        label="Topic"
-        value={topic}
-        onChange={onInputChange}
-      />
-      <ArticlesList />
+      <Box
+        sx={{
+          height: '10vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'end',
+        }}
+      >
+        <Typography component="h1" variant="h2" gutterBottom>
+          Articles Seeker
+        </Typography>
+        <TextField
+          fullWidth
+          label="Topic"
+          value={topic.value}
+          onChange={onInputChange}
+          sx={{ mb: 2 }}
+        />
+      </Box>
+      <Box
+        sx={{
+          height: '60vh',
+          overflow: 'scroll',
+          border: '1px solid',
+          borderColor: 'grey.300',
+          borderRadius: 1,
+          py: 2,
+          px: 5,
+        }}
+      >
+        <ArticlesList
+          articles={data?.articles}
+          topic={data?.topic}
+          isLoading={isLoading}
+          error={error}
+        />
+      </Box>
     </Box>
   );
 }
--- src/components/ArticlesList.tsx
+++ src/components/ArticlesList.tsx
@@ -1,13 +1,45 @@
-import { Box } from '@mui/material';
 import Article from './Article';
+import { IArticle } from '../types/article';
+import ArticleSkeleton from './ArticleSkeleton';
 
-function ArticlesList() {
+interface IArticleProps {
+  articles?: IArticle[];
+  topic?: string;
+  isLoading: boolean;
+  error: string | null;
+}
+
+function ArticlesList({ articles, topic, isLoading, error }: IArticleProps) {
+  if (isLoading) {
+    return (
+      <>
+        <ArticleSkeleton />
+      </>
+    );
+  }
+
+  if (error) {
+    return error;
+  }
+
+  if (!topic) {
+    return "Start typing and we'll find the best articles for you";
+  }
+
+  if (!articles?.length) {
+    return 'Sorry, we could not find any match... Please try other topic';
+  }
+
   return (
-    <Box>
-      {[1, 2, 3, 4, 5].map(() => (
-        <Article />
+    <>
+      {articles.map((article) => (
+        <Article
+          key={article.id}
+          article={article}
+          sx={{ mb: 2, ':last-child': { mb: 0 }, textAlign: 'left' }}
+        />
       ))}
-    </Box>
+    </>
   );
 }
 
