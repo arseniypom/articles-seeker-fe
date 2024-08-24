07c4ef72db2093036476e210039228724f19aed2
arseniypom <arpomazkov@bk.ru>
2024-08-24 16:21:55 +0300

    Added loading skeleton

---
+++ src/components/ArticleSkeleton.tsx
@@ -0,0 +1,21 @@
+import { Card, CardActions, CardContent, Skeleton, Stack } from '@mui/material';
+
+function ArticleSkeleton() {
+  return (
+    <Card variant="outlined">
+      <CardContent>
+        <Stack spacing={1}>
+          <Skeleton variant="rectangular" width={50} height={14} />
+          <Skeleton variant="rectangular" width="full" height={60} />
+          <Skeleton variant="rectangular" width="full" height={14} />
+          <Skeleton variant="rectangular" width="full" height={14} />
+        </Stack>
+      </CardContent>
+      <CardActions sx={{ px: 2 }}>
+        <Skeleton variant="rounded" width={85} height={30} />
+      </CardActions>
+    </Card>
+  );
+}
+
+export default ArticleSkeleton;
